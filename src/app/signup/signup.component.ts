4c48398b582e1efc0283995fe9d9e62967143a25
anasatmani8 <anasatmani8@gmail.com>
2023-01-02 16:50:51 +0100

    Signup Rest connected(F.E/B.E)

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -18,6 +18,35 @@ export class SignupComponent implements OnInit {
   confirmPassword = true;
   signupForm:any = FormGroup;
   responseMessage: any;
+
+  formErrors : { [char: string]: string } = {
+    'name': '',
+    'contactNumber': '',
+    'email': '',
+    'password': ''
+  } as const;
+
+  validationMessages : any = {
+      'name': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+
+    'contactNumber': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email not in valid format.'
+    },
+    'password': {
+      'required':      'Password is required',
+      'minlength':     'Password must be at least 5 charachters long',
+      'maxlength':     'Password cannot be more than 100 charachters long'
+    }
+  };
   constructor(private formBuilder: FormBuilder,
     private router: Router,
     private userService: UserService,
@@ -27,12 +56,37 @@ export class SignupComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
-      name:[null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
-      email:[null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      name:[null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex), Validators.minLength(2), Validators.maxLength(25)]],
+      email:[null, [Validators.required, Validators.email, Validators.pattern(GlobalConstants.emailRegex)]],
       contactNumber:[null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
       password:[null, [Validators.required]],
       confirmPassword:[null, [Validators.required]],
     })
+    this.signupForm.valueChanges
+    .subscribe((data: any) => this.onValueChanged(data));
+
+  this.onValueChanged(); // (re)set validation messages now
+
+  }
+  onValueChanged(data?: any) {
+    if (!this.signupForm) { return; }
+    const form = this.signupForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   validateSubmit(){
@@ -70,6 +124,7 @@ export class SignupComponent implements OnInit {
         this.responseMessage = GlobalConstants.genericError;
       }
       this.snackBarService.openSnackbar(this.responseMessage, GlobalConstants.error);
+      console.log(this.responseMessage, GlobalConstants.error);
     })
   }
 
