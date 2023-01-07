51b652fb53a25599f2ef8bef8fbdc9481aeb89ec
anasatmani8 <anasatmani8@gmail.com>
2023-01-07 16:03:39 +0100

    Change Password F.E v1.3

--- src/app/material-component/dialog/change-password/change-password.component.ts
+++ src/app/material-component/dialog/change-password/change-password.component.ts
@@ -12,8 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.scss'],
 })
 export class ChangePasswordComponent implements OnInit {
-  changePasswordForm: any = FormGroup;
+  newPassword = true;
+  confirmPassword = true;
+  oldPassword = true;
+  changePasswordForm:any = FormGroup;
   responseMessage: any;
+
+  formErrors : { [char: string]: string } = {
+    'newPassword': '',
+    'confirmPassword': '',
+    'oldPassword': ''
+  } as const;
+
+  validationMessages : any = {
+    'newPassword': {
+      'required':      'Password is required',
+      'minlength':     'Password must be at least 2 charachters long',
+      'maxlength':     'Password cannot be more than 100 charachters long'
+    },
+    'confirmPassword': {
+      'required':      'Password is required',
+      'minlength':     'Password must be at least 2 charachters long',
+      'maxlength':     'Password cannot be more than 100 charachters long'
+    },
+    'oldPassword': {
+      'required':      'Password is required',
+      'minlength':     'Password must be at least 2 charachters long',
+      'maxlength':     'Password cannot be more than 100 charachters long'
+    }
+  };
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
@@ -24,10 +51,35 @@ export class ChangePasswordComponent implements OnInit {
 
   ngOnInit(): void {
     this.changePasswordForm = this.formBuilder.group({
-      oldPassword: [null, [Validators.required]],
-      newPassword: [null, [Validators.required]],
-      confirmPassword: [null, [Validators.required]],
+      oldPassword: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      newPassword: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      confirmPassword: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(25)]]
     });
+    this.changePasswordForm.valueChanges
+    .subscribe((data: any) => this.onValueChanged(data));
+
+  this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.changePasswordForm) { return; }
+    const form = this.changePasswordForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   validateSubmit() {
@@ -41,29 +93,34 @@ export class ChangePasswordComponent implements OnInit {
     }
   }
 
-  handelChangePasswordSubmit(){
+  handelChangePasswordSubmit() {
     this.ngxService.show();
     var formData = this.changePasswordForm.value;
     var data = {
-      oldPassword : formData.oldPassword,
-      newPassword : formData.newPassword,
-      confirmPassword : formData.confirmPassword
-    }
-    this.userService.chnagePassword(data).subscribe((response:any)=>{
-      this.ngxService.hide();
-      this.responseMessage = response?.message;
-      this.dialogRef.close();
-      this.snackbarService.openSnackbar(this.responseMessage,"Success");
-    }, (error)=>{
-      console.log(error);
-      this.ngxService.hide();
-      if (error.error?.message) {
-        this.responseMessage = error.error?.message;
-
-      } else {
-        this.responseMessage =  GlobalConstants.genericError;
+      oldPassword: formData.oldPassword,
+      newPassword: formData.newPassword,
+      confirmPassword: formData.confirmPassword,
+    };
+    this.userService.chnagePassword(data).subscribe(
+      (response: any) => {
+        this.ngxService.hide();
+        this.responseMessage = response?.message;
+        this.dialogRef.close();
+        this.snackbarService.openSnackbar(this.responseMessage, 'Success');
+      },
+      (error) => {
+        console.log(error);
+        this.ngxService.hide();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackbar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
       }
-      this.snackbarService.openSnackbar(this.responseMessage, GlobalConstants.error);
-    })
+    );
   }
 }
