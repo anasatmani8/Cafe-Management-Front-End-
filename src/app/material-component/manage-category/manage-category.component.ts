f5eabc1907839f2a5930bc69ef891839bdc67b0e
anasatmani8 <anasatmani8@gmail.com>
2023-01-07 19:51:01 +0100

    Categorys F.E v1.1

---
+++ src/app/material-component/manage-category/manage-category.component.ts
@@ -0,0 +1,61 @@
+import { filter } from 'rxjs/operators';
+import { GlobalConstants } from './../../shared/global-constants';
+import { SnackbarService } from './../../services/snackbar.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
+import { CategoryService } from './../../services/category.service';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-manage-category',
+  templateUrl: './manage-category.component.html',
+  styleUrls: ['./manage-category.component.scss']
+})
+export class ManageCategoryComponent implements OnInit {
+
+
+  displayedColumns:string[]= ['name', 'edit '];
+  dataSource:any;
+  responseMessage:any;
+  constructor(private categoryService:CategoryService,
+    private router:Router,
+    private ngxSpinnerService:NgxSpinnerService,
+    private snackbarService:SnackbarService,
+    private dialog:MatDialog) { }
+
+  ngOnInit(): void {
+    this.ngxSpinnerService.show();
+    this.tableData();
+  }
+
+  tableData(){
+    console.log('start getting categorys')
+    this.categoryService.getCategorys().subscribe((response:any)=>{
+      this.ngxSpinnerService.hide();
+      this.dataSource = new MatTableDataSource(response);
+
+    },(error)=>{
+      this.ngxSpinnerService.hide();
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      }
+      else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackbar(this.responseMessage, GlobalConstants.error);
+    })
+
+  }
+
+  applyFilter(event:Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handeAddActions() {}
+
+  handelEditActions(value:any){}
+
+}
--- src/app/material-component/material.routing.ts
+++ src/app/material-component/material.routing.ts
@@ -1,6 +1,16 @@
+import { RouteGuardService } from './../services/route-guard.service';
 import { Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ManageCategoryComponent } from './manage-category/manage-category.component';
 
 
 export const MaterialRoutes: Routes = [
+  {
+    path:'category',
+    component:ManageCategoryComponent,
+    canActivate:[RouteGuardService],
+    data:{
+      expectedRole:['admin']
+    }
+  }
 ];
